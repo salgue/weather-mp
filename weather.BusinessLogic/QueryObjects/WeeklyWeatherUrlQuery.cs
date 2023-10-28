c877e4b9dab2320c51353f83539b9621444918e1
Missael Padilla <lmpadillar@gmail.com>
2023-10-27 20:41:21 -0700

    Create Query objects and business logic

---
+++ weather.BusinessLogic/QueryObjects/WeeklyWeatherUrlQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather.BusinessLogic.QueryObjects
+{
+    public class WeeklyWeatherUrlQuery
+    {
+        public string SearchValue { get; set; }
+    }
+}
